eb9103336903294a1277cb2b15024d5ee0bec03d
PunyTitan <pinjiahe@gmail.com>
2017-02-09 12:49:49 +0800

    Add parameters for different data sets

--- demo/example1.py
+++ demo/example1.py
@@ -20,3 +20,70 @@ for i in range(0,10,1):
 	pprint(result)
 	savetxt('10experiment_withRE'+dataName[curData]+'.csv',result,delimiter=',')
 
+#IPLoM
+#For 2kHDFS data:
+#       (self,path='../Data/2kHDFS/',logname='NoID_2kHDFS.log',removable=True,removeCol=[0,1,2,3,4],regular=True,
+#		rex=['blk_(|-)[0-9]+','(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)'],savePath='./results_2kHDFS/',saveFileName='template',groupNum=14):
+#For 2kZookeeper:
+# 		(self,path='../Data/2kZookeeper/',logname='NoID_2kZookeeper.log',removable=True,removeCol=[0,1,2,3,4,5],regular=True,
+# 		rex=['(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)'],savePath='./results_2kZookeeper/',saveFileName='template',groupNum=46):
+#For 2kBGL: 
+#		(self,path='../Data/2kBGL/',logname='NoID_2kBGL.log',removable=True,removeCol=[0,1,2,3,4,5,6,7,8],regular=True,
+#		rex=['core\.[0-9]*'],savePath='./results_2kBGL/',saveFileName='template',groupNum=100):# line 55,change the regular expression replacement code
+#For 2kHPC:
+#		(self,path='../Data/2kHPC/',logname='NoID_2kHPC.log',removable=True,removeCol=[0,1],regular=True,
+#		rex=['([0-9]+\.){3}[0-9]'],savePath='./results_2kHPC/',saveFileName='template',groupNum=51):# line 55,change the regular expression replacement code
+#For 2kProxifier:
+#       (self,path='../Data/2kProxifier/',logname='NoID_2kProxifier.log',removable=False,removeCol=[0,1,3,4],regular=True,
+#		rex=[''],savePath='./results_2kProxifier/',saveFileName='template',groupNum=6):
+
+#LogSig
+#**********************PARAMETERS SETTING**************************************************
+# Replace the parameters of def __init__ with the following ones according to the dataset.
+# Please be noted that part of the codes in function termpairGene need to be altered according to the dataset
+#******************************************************************************************
+# =====For BGL=====
+# (self,path='../Data/2kBGL/',logname='rawlog.log',removable=True,removeCol=[0,1,2,3,4,5,6,7,8,9],regular=True,
+# rex=['core\.[0-9]*'],savePath='./results_2kBGL/',saveFileName='template',groupNum=100):# line 66,change the regular expression replacement code
+# =====For Proxifier=====
+# (self,path='../Data/2kProxifier/',logname='rawlog.log',removable=True,removeCol=[0,1,2,4,5],regular=True,
+# rex=[''],savePath='./results_2kProxifier/',saveFileName='template',groupNum=6):
+# =====For Zookeeper=====
+# (self,path='../Data/2kZookeeper/',logname='rawlog.log',removable=True,removeCol=[0,1,2,3,4,5,6],regular=True,
+# rex=['(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)'],savePath='./results_2kZookeeper/',saveFileName='template',groupNum=46):
+# =====For HDFS=====
+# (self,path='../Data/2kHDFS/',logname='rawlog.log',removable=True,removeCol=[0,1,2,3,4,5],regular=True,
+# rex=['blk_(|-)[0-9]+','(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)'],savePath='./results_2kHDFS/',saveFileName='template',groupNum=14):
+# =====For HPC=====
+# (self,path='../Data/2kHPC/',logname='rawlog.log',removable=True,removeCol=[0,1],regular=True,
+# rex=['([0-9]+\.){3}[0-9]'],savePath='./results_2kHPC/',saveFileName='template',groupNum=51):# line 67,change the regular expression replacement code
+#******************************************************************************************
+
+#LKE
+#**********************PARAMETERS SETTING**************************************************
+# Replace the parameters of def __init__ with the following ones according to the dataset.
+# Please be noted that part of the codes in function termpairGene need to be altered according to the dataset
+#******************************************************************************************
+# =====For BGL=====
+# (self,path='../Sca/',dataName='Sca_BGL600',logname='rawlog.log',removable=True,removeCol=[0,1,2,3,4,5,6,7,8,9],threshold2=5,regular=True,
+# rex=['core\.[0-9]*'],savePath='./results/',saveFileName='template'):# line 67,change the regular expression replacement code
+# =====For Proxifier=====
+# (self,path='../Sca/',dataName='Sca_Proxifier600',logname='rawlog.log',removable=True,removeCol=[0,1,2,4,5],regular=True,threshold2=2,
+# rex=[''],savePath='./results/',saveFileName='template'):
+# =====For Zookeeper=====
+# (self,path='../Sca/',dataName='Sca_Zookeeper600',logname='rawlog.log',removable=True,removeCol=[0,1,2,3,4,5,6],threshold2=2,regular=True,
+# rex=['(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)'],savePath='./results/',saveFileName='template'):
+# =====For HDFS=====
+# (self,path='../Sca/',dataName='Sca_SOSP600',logname='rawlog.log',removable=True,removeCol=[0,1,2,3,4,5],threshold2=3,regular=True,
+# rex=['blk_(|-)[0-9]+','(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)'],savePath='./results/',saveFileName='template'):
+# =====For HPC=====
+# (self,path='../Sca/',dataName='Sca_HPC600',logname='rawlog.log',removable=True,removeCol=[0,1],threshold2=4,regular=True,
+# rex=['([0-9]+\.){3}[0-9]'],savePath='./results/',saveFileName='template'):# line 67,change the regular expression replacement code
+#******************************************************************************************
+
+#SLCT
+#For SOSP: support is 12,False
+#For HPC: support is 49, True
+#For BGL: support is 5, True
+#For Proxifier: support is 29, True
+#For Zookeeper: support is 9, True
