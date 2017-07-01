eff4c27df07703591f5f1226262da52df5dc179a
PunyTitan <pinjiahe@gmail.com>
2017-07-01 16:30:59 +0800

    Add explanations on delimiters

--- logparser/POP.py
+++ logparser/POP.py
@@ -44,7 +44,7 @@ writeTemplate = open(templateFile, 'w')
 regexL = []
 specialL = []
 specialNum = 0
-delimiters = ' ' #If you want multiple delimiters, for example, ';' ',' '*' '\n', them delimiter='; |, |\*|\n'
+delimiters = ' ' #If you want multiple delimiters, for example, to split "field0=value0 field1:value1", them delimiter='=|:| '
 printStructuredLogs = True
 
 
