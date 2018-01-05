f3f4321448ab4535468ccd5bd7c49f6a7edaacb3
Jinyang88 <liujy725@gmail.com>
2018-01-05 10:42:39 +0800

    Add LogSig demo

---
+++ demo/LogSig_demo.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('../LogSig')
+sys.path.append('../logparser')
+
+from LogSig import *
+import evaluator
+
+input_dir = '../logs/HDFS/'
+output_dir = 'LogSig_result/'
+
+log_file = 'HDFS_2k.log'
+log_format = 'Date Time Pid Level Component: Content' # HDFS log format
+
+regex = ['blk_(|-)[0-9]+', '(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)']
+
+myparser=LogSig(path=input_dir, savePath=output_dir, rex=regex,
+                groupNum=14, logformat=log_format)
+myparser.parse(log_file)
+
+evaluator.evaluate(os.path.join(input_dir, 'HDFS_2k.log_structured.csv'),
+                   os.path.join(output_dir, 'HDFS_2k.log_structured.csv'))
--- demo/example1_logparser_singlethread.py
+++ demo/example1_logparser_singlethread.py
@@ -3,15 +3,11 @@ import sys
 sys.path.append('../logparser')
 import logparser
 
-input_dir = '../logs/HPC/'
+input_dir = '../logs/HDFS/'
 output_dir = 'result/'
 
-log_file = 'HPC_2k.log'
-log_format = 'ID Node Component State Time Flag Content' # HPC log format
-# log_format = 'Month Date Time Level Component: Content' # Linux log format
-# log_format = '\[Time\] \[Level\] Content' # Apache log format
-# log_format = 'Date Time, Level                  Component    Content' # Windows log format
-# log_format = 'Date Time Pid Level Component: Content' # HDFS log format
+log_file = 'HDFS_2k.log'
+log_format = 'Date Time Pid Level Component: Content' # HDFS log format
 
 parser = logparser.Parser(logformat=log_format, indir=input_dir, outdir=output_dir)
 parser.parse(log_file)
