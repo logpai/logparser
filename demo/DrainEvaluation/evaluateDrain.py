851253549a69b4f5b16f6f6713f6fd9ca8d38d6b
Pinjia He <pinjiahe@gmail.com>
2018-12-30 19:18:25 +0100

    Update evaluateDrain.py

--- demo/DrainEvaluation/evaluateDrain.py
+++ demo/DrainEvaluation/evaluateDrain.py
@@ -1,59 +1,27 @@
-#!/usr/bin/env python
+from Drainjournal import *
 from pprint import pprint
 from RI_precision import *
-from LogSig import *
-import gc
-import numpy as np
+
+delimiters = '\s+'
+
+dataPath = './data/HDFS/'
+removeCol = [0,1,2,3,4]
+rex = [('blk_(|-)[0-9]+', 'blkID'), ('(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)', 'IPAddandPortID')]
+mt = 1
+delimiters = '\s+'
 
 
-dataset = 3
-dataPath = './data/'
+logName = dataPath + 'rawlog.log'
+myPara = Para(logName=logName, removeCol=removeCol, rex=rex, delimiters=delimiters, mt=mt)
 
-if dataset == 1:
-	dataName = 'BGL'
-	groupNum = 100
-	removeCol = [0,1,2,3,4,5,6,7,8,9]
-	regL = ['core\.[0-9]*']
-	# regL = []
-elif dataset == 2:
-	dataName = 'HPC'
-	groupNum = 51
-	removeCol = [0,1]
-	regL = ['([0-9]+\.){3}[0-9]']
-	# regL = []
-elif dataset == 3:
-	dataName = 'HDFS'
-	groupNum = 14
-	removeCol = [0,1,2,3,4,5]
-	regL = ['blk_(|-)[0-9]+','(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)']
-	# regL = [] 
-elif dataset == 4:
-	dataName = 'Zookeeper'
-	groupNum = 46
-	removeCol = [0,1,2,3,4,5,6]
-	regL = ['(/|)([0-9]+\.){3}[0-9]+(:[0-9]+|)(:|)']
-	# regL = []
-elif dataset == 5:
-	dataName = 'Proxifier'
-	groupNum = 6
-	removeCol = [0,1,2,4,5]
-	regL = [] 
+myParser = Drain(myPara)
+runningTime = myParser.mainProcess()
 
 
-result = np.zeros((1,9))
-
-for i in range(0,1,1):
-	print ('the ', i+1, 'th experiment starts here!')
-	parserPara = Para(path=dataPath+dataName+'/', groupNum=groupNum, removeCol=removeCol, rex=regL, savePath='./results/')
-	myParser = LogSig(parserPara)
-	runningTime = myParser.mainProcess()
-
-	parameters=prePara(groundTruthDataPath=dataPath+dataName+'/', geneDataPath='./results/')
-
-	TP,FP,TN,FN,p,r,f,RI=process(parameters)
-	result[i,:]=TP,FP,TN,FN,p,r,f,RI,runningTime
+#Evaluate code
+result = zeros((1,9))
+evaPara = prePara(groundTruthDataPath=dataPath, geneDataPath='./results/')
+TP,FP,TN,FN,p,r,f,RI=process(evaPara)
+result[0,:]=TP,FP,TN,FN,p,r,f,RI,runningTime
 	
-	pprint(result)
-
-	gc.collect()
-
+pprint(result)
