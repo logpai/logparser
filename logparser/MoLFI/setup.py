1080726933c1a40dfc4ad16d6db3735f091ba997
Pinjia He <pinjiahe@gmail.com>
2018-12-30 18:42:56 +0100

    Create setup.py

---
+++ logparser/MoLFI/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MoLFI.py',
+      version='1.0.0',
+      description='Multi-Objective Log message Format Identification',
+      author='Salma Messaoudi',
+      author_email='messaoudi.salma.lu@gmail.com',
+      url='https://github.com/SalmaMessaoudi/MoLFI.git',
+      packages=['main', 'test'],
+     )
