c4a09d15fc44f4bc56ed94cbf3785fbb01d60ab2
Johannes Moest <johannes.moest@nokia.com>
2022-08-16 16:44:00 +0200

    Spell.py fix for Python 3.9

--- logparser/Spell/Spell.py
+++ logparser/Spell/Spell.py
@@ -309,7 +309,7 @@ class LogParser:
         regex = ''
         for k in range(len(splitters)):
             if k % 2 == 0:
-                splitter = re.sub(' +', '\s+', splitters[k])
+                splitter = re.sub(' +', '\\\s+', splitters[k])
                 regex += splitter
             else:
                 header = splitters[k].strip('<').strip('>')
