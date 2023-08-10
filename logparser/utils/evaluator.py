26bb8bd61fbc2b849b16f1932be778e848ed3bf6
zzkluck <zzkluck@outlook.com>
2023-08-10 13:19:01 +0800

    Modified the 'logparser/utils/evaluator.py'.
    Modified the 'logparser/utils/evaluator.py' so that it no longer relies on the huge libraries 'scipy' and 'pandas'. 
    It now only includes the native python modules, which will be beneficial for standalone operation.

--- logparser/utils/evaluator.py
+++ logparser/utils/evaluator.py
@@ -1,22 +1,27 @@
 """
-Description : This file implements the function to evaluation accuracy of log parsing
+Description : This file implements the function to evaluation accuracy of log parsing.
+              Modified by zzkluck for stand-alone usage, involve only built-in python modules.
 Author      : LogPAI team
+Modifier    : zzkluck
 License     : MIT
 """
 
-import sys
-import pandas as pd
-from collections import defaultdict
-import scipy.misc
+import csv
+from typing import Tuple, List
+from collections import defaultdict, Counter
+from math import comb
+
+# when use python version < 3.8, use following comb_2(n) instead of math.comb(n, 2)
+# def comb_2(n: int): return n * (n - 1) // 2
 
 
-def evaluate(groundtruth, parsedresult):
+def evaluate(groundtruth: str, parsedresult: str) -> Tuple[float, float]:
     """ Evaluation function to benchmark log parsing accuracy
-    
+
     Arguments
     ---------
         groundtruth : str
-            file path of groundtruth structured csv file 
+            file path of groundtruth structured csv file
         parsedresult : str
             file path of parsed structured csv file
 
@@ -24,28 +29,42 @@ def evaluate(groundtruth, parsedresult):
     -------
         f_measure : float
         accuracy : float
-    """ 
-    df_groundtruth = pd.read_csv(groundtruth)
-    df_parsedlog = pd.read_csv(parsedresult)
-    # Remove invalid groundtruth event Ids
-    non_empty_log_ids = df_groundtruth[~df_groundtruth['EventId'].isnull()].index
-    df_groundtruth = df_groundtruth.loc[non_empty_log_ids]
-    df_parsedlog = df_parsedlog.loc[non_empty_log_ids]
-    (precision, recall, f_measure, accuracy) = get_accuracy(df_groundtruth['EventId'], df_parsedlog['EventId'])
-    print('Precision: %.4f, Recall: %.4f, F1_measure: %.4f, Parsing_Accuracy: %.4f'%(precision, recall, f_measure, accuracy))
+    """
+    invalid_line_id = set()
+    ground_truth_event_id = []
+    parsed_result_event_id = []
+
+    with open(groundtruth, 'r') as gt_file:
+        reader = csv.DictReader(gt_file)
+        for i, line in enumerate(reader):
+            if line['EventId'] == "":
+                invalid_line_id.add(i)
+            else:
+                ground_truth_event_id.append(line['EventId'])
+
+    with open(parsedresult, 'r') as pr_file:
+        reader = csv.DictReader(pr_file)
+        for i, line in enumerate(reader):
+            if i not in invalid_line_id:
+                parsed_result_event_id.append(line['EventId'])
+
+    (precision, recall, f_measure, accuracy) = get_accuracy(ground_truth_event_id, parsed_result_event_id)
+    print(f'Precision: {precision:.4f}, Recall: {recall:.4f}, '
+          f'F1_measure: {f_measure:.4f}, Parsing_Accuracy: {accuracy:.4f}')
     return f_measure, accuracy
 
-def get_accuracy(series_groundtruth, series_parsedlog, debug=False):
+
+def get_accuracy(ground_truth: List[str], parsed_result: List[str], debug=None) -> Tuple[float, float, float, float]:
     """ Compute accuracy metrics between log parsing results and ground truth
     
     Arguments
     ---------
-        series_groundtruth : pandas.Series
+        ground_truth : List[str]
             A sequence of groundtruth event Ids
-        series_parsedlog : pandas.Series
+        parsed_result : List[str]
             A sequence of parsed event Ids
-        debug : bool, default False
-            print error log messages when set to True
+        debug : None
+            Deprecated in this version
 
     Returns
     -------
@@ -54,45 +73,29 @@ def get_accuracy(series_groundtruth, series_parsedlog, debug=False):
         f_measure : float
         accuracy : float
     """
-    series_groundtruth_valuecounts = series_groundtruth.value_counts()
-    real_pairs = 0
-    for count in series_groundtruth_valuecounts:
-        if count > 1:
-            real_pairs += scipy.misc.comb(count, 2)
+    gt_counter = defaultdict(list)
+    for i, eventId in enumerate(ground_truth):
+        gt_counter[eventId].append(i)
+    real_pairs = sum(comb(len(ids), 2) for ids in gt_counter.values())
 
-    series_parsedlog_valuecounts = series_parsedlog.value_counts()
-    parsed_pairs = 0
-    for count in series_parsedlog_valuecounts:
-        if count > 1:
-            parsed_pairs += scipy.misc.comb(count, 2)
-
+    pr_counter = defaultdict(list)
+    for i, eventId in enumerate(parsed_result):
+        pr_counter[eventId].append(i)
+    parsed_pairs = sum(comb(len(ids), 2) for ids in pr_counter.values())
     accurate_pairs = 0
-    accurate_events = 0 # determine how many lines are correctly parsed
-    for parsed_eventId in series_parsedlog_valuecounts.index:
-        logIds = series_parsedlog[series_parsedlog == parsed_eventId].index
-        series_groundtruth_logId_valuecounts = series_groundtruth[logIds].value_counts()
-        error_eventIds = (parsed_eventId, series_groundtruth_logId_valuecounts.index.tolist())
-        error = True
-        if series_groundtruth_logId_valuecounts.size == 1:
-            groundtruth_eventId = series_groundtruth_logId_valuecounts.index[0]
-            if logIds.size == series_groundtruth[series_groundtruth == groundtruth_eventId].size:
-                accurate_events += logIds.size
-                error = False
-        if error and debug:
-            print('(parsed_eventId, groundtruth_eventId) =', error_eventIds, 'failed', logIds.size, 'messages')
-        for count in series_groundtruth_logId_valuecounts:
+    accurate_events = 0  # determine how many lines are correctly parsed
+    for parsed_eventId in pr_counter.keys():
+        error_counter = Counter(ground_truth[i] for i in pr_counter[parsed_eventId])
+        if len(error_counter) == 1:
+            ground_truth_eventId = next(iter(error_counter.keys()))
+            if len(gt_counter[ground_truth_eventId]) == len(pr_counter[parsed_eventId]):
+                accurate_events += len(pr_counter[parsed_eventId])
+        for count in error_counter.values():
             if count > 1:
-                accurate_pairs += scipy.misc.comb(count, 2)
+                accurate_pairs += comb(count, 2)
 
     precision = float(accurate_pairs) / parsed_pairs
     recall = float(accurate_pairs) / real_pairs
     f_measure = 2 * precision * recall / (precision + recall)
-    accuracy = float(accurate_events) / series_groundtruth.size
+    accuracy = float(accurate_events) / len(ground_truth)
     return precision, recall, f_measure, accuracy
-
-
-
-
-
-
-
