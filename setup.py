33c231cbf39601977f25c74673a8452298bf79f6
zhujiem <zhujiem@users.noreply.github.com>
2023-09-12 13:05:40 +0800

    Release logparser v1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fd:
 
 setuptools.setup(
     name="logparser3",
-    version="1.0.2",
+    version="1.0.3",
     author="logpai",
     author_email="logpai@users.noreply.github.com",
     description="A machine learning toolkit for log parsing from LOGPAI",
